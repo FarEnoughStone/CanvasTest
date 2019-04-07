d80fe2932cb89790b2049e87153521039d59ad70
hongkui <hongkui.zhen@gmail.com>
2019-04-08 00:44:39 +0800

    openGL

--- app/src/main/java/space/hongkui/canvastest/MainActivity.java
+++ app/src/main/java/space/hongkui/canvastest/MainActivity.java
@@ -1,9 +1,12 @@
 package space.hongkui.canvastest;
 
 import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+
+    GLSurfaceView mGLSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/space/hongkui/canvastest/TestGLSurfaceView.java
@@ -0,0 +1,18 @@
+package space.hongkui.canvastest;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+
+public class TestGLSurfaceView extends GLSurfaceView {
+
+    public TestGLSurfaceView(Context context) {
+        super(context);
+    }
+
+    public TestGLSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
