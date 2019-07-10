b32c74b2f179ebfa07dfdeca4732413c4efbc076
hongkui <hongkui.zhen@gmail.com>
2019-07-11 03:34:53 +0800

    OpenGL

--- app/src/main/java/space/hongkui/canvastest/TestGLSurfaceView.java
+++ app/src/main/java/space/hongkui/canvastest/TestGLSurfaceView.java
@@ -4,15 +4,44 @@ import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.util.AttributeSet;
 
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
 public class TestGLSurfaceView extends GLSurfaceView {
 
     public TestGLSurfaceView(Context context) {
         super(context);
+        init();
     }
 
     public TestGLSurfaceView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
+    private void init() {
+        setEGLContextClientVersion(2);
+        setRenderer(new TestRenderer());
+    }
+
+    //渲染器
+    class TestRenderer implements Renderer {
+
+        @Override
+        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+        }
+
+        //设置视图大小
+        @Override
+        public void onSurfaceChanged(GL10 gl, int width, int height) {
+            gl.glViewport(0, 0, width, height);
+        }
+
+        @Override
+        public void onDrawFrame(GL10 gl) {
+
+        }
+    }
 
 }
